0a0f335db74c98f5202d2c6760fa464d81ca5ac1
tasib94 <rahi84974@gmail.com>
2024-03-15 01:37:08 +0600

    learned about useState hook

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Counter from './Counter'
+
+function App() {
+  
+  return (
+    <>
+      <h3>React core concepts part-2</h3>
+      <Counter></Counter>
+    </>
+  )
+}
+
+export default App
