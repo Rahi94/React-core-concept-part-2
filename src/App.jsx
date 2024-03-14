4f26092700a35db9318441c358a3813f6885bc5c
tasib94 <rahi84974@gmail.com>
2024-03-15 02:56:00 +0600

    explored useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import './App.css'
 import Counter from './Counter'
 import Players from './Team'
+import Users from './Users'
 
 function App() {
   
   return (
     <>
       <h3>React core concepts part-2</h3>
+      <Users></Users>
       <Players></Players>
       <Counter></Counter>
     </>
